20b8f075b857ccca50e851a08a3da53a74e6f18e
Rajesh B <117339281+rb4807@users.noreply.github.com>
2023-11-10 10:03:41 +0530

    Update settings.py

--- mfj/settings.py
+++ mfj/settings.py
@@ -32,8 +32,7 @@ EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['.vercel.app']
 
 # Application definition
 
